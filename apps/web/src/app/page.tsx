2958dfad01f9097b015e0b002f8d58d7702c7544
Prasad Domala <prasad.domala@kheriox.tech>
2026-03-04 17:32:41 +1000

    Added page heading

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,14 @@
 import { env } from '@repo/env/web';
+import PageHeading from '@/components/page-heading';
 import { cn } from '@/lib/utils';
 
 export default function Home() {
 	return (
 		<div className={cn('flex h-full flex-col')}>
-			<h1>{env.NEXT_PUBLIC_APP_NAME}</h1>
+			<PageHeading
+				description="Welcome to the home page"
+				heading={`${env.NEXT_PUBLIC_APP_NAME} Home`}
+			/>
 		</div>
 	);
 }
