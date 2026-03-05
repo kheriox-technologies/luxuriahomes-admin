626b3486e0b03911a3bb20a2c35ba889bef35adc
Prasad Domala <prasad.domala@kheriox.tech>
2026-03-05 14:23:43 +1000

    Added auth actions for web app

--- apps/web/app/(authenticated)/layout.tsx
+++ apps/web/app/(authenticated)/layout.tsx
@@ -1,9 +1,15 @@
+import { redirect } from 'next/navigation';
+import { hasAppAccess } from '@/actions/auth';
 import AuthGuard from '@/guards/auth-guard';
 
 type AuthenticatedLayoutProps = Readonly<{
 	children: React.ReactNode;
 }>;
-const AuthenticatedLayout = ({ children }: AuthenticatedLayoutProps) => {
+const AuthenticatedLayout = async ({ children }: AuthenticatedLayoutProps) => {
+	const hasAccess = await hasAppAccess();
+	if (!hasAccess) {
+		redirect('/error?error=arbitrary_octopus');
+	}
 	return (
 		<AuthGuard>
 			<div className="flex flex-col overflow-x-hidden">{children}</div>
